40e4e4b8a55bd8b1a3dc0d53fadff068a2b2e97c
Vlados <vladyslavkutsyi@seznam.cz>
2023-08-08 08:24:03 +0300

    Commit my Homework 42 Asynchronous tasks in Django.Homework fix

--- django/myproject/celery.py
+++ django/myproject/celery.py
@@ -1,10 +1,32 @@
 import os
+import logging
 from celery import Celery
 
+# Налаштування для логування Celery в файл
+log_file = 'celery.log'
+log_level = logging.INFO  # або інший рівень логування
 
+# Налаштування логгера Celery
+celery_logger = logging.getLogger('celery')
+celery_logger.setLevel(log_level)
+
+# Додаємо обробник для запису логів у файл
+file_handler = logging.FileHandler(log_file)
+file_handler.setLevel(log_level)
+file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+celery_logger.addHandler(file_handler)
+
+# Налаштування Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 app = Celery('myproject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
+
+# Функція для логування інформації про завдання
+@app.task(bind=True)
+def debug_task(self):
+    print('Завдання виконано: {0!r}'.format(self.request))
--- django/user/tasks.py
+++ django/user/tasks.py
@@ -7,21 +7,29 @@ from purchase.models import Purchase
 @shared_task
 def send_welcome_email():
     # Імітуємо відправку ласкаво просимо на сайт листа з привітанням.
-    time.sleep(5)
-    print("Welcome to Celery!")
+    # time.sleep(5)
+    result = "Welcome email sent!"
+    print(result)
+    return result
 
 
 @shared_task
 def print_purchase_count(user_id):
     try:
         user = CustomUser.objects.get(pk=user_id)
-        purchase_count = Purchase.objects.filter(user=user).count()
-        print(f"User {user.username} has {purchase_count} purchases.")
+        purchase_count = Purchase.objects.filter(user__id=user_id).count()
+        result = f"User {user.username} has {purchase_count} purchases."
+        print(result)
+        return result
     except CustomUser.DoesNotExist:
-        print(f"User with ID {user_id} does not exist.")
+        result = f"User with ID {user_id} does not exist."
+        print(result)
+        return result
 
 
 @shared_task
 def print_user_count():
     user_count = CustomUser.objects.count()
-    print(f"Number of users in the database: {user_count}")
+    result = f"Number of users in the database: {user_count}"
+    print(result)
+    return result
